800b3c83c37ff56ceec75128cb2997db6ca8f4a5
gabo19-01 <gabrielgranadosc@gmail.com>
2023-03-15 21:12:52 -0600

    Class 9-2
    2 columns added.
    1 moving platform to move between the columns.

---
+++ TrabajoClase3D/Assets/Scripts/MoveWithFloor.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWithFloor : MonoBehaviour
+{
+    CharacterController player;
+
+    Vector3 groundPosition;
+    Vector3 lastGroundPosition;
+    string groundName;
+    string lastGroundName;
+
+    void Start()
+    {
+        player = this.GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (player.isGrounded)
+        {
+            RaycastHit hit;
+            if (Physics.SphereCast(transform.position, player.height / 4.2f, -transform.up, out hit))
+            {
+                GameObject groundedIn = hit.collider.gameObject;
+                groundName = groundedIn.name;
+                groundPosition = groundedIn.transform.position;
+                if (groundPosition != lastGroundPosition && groundName == lastGroundName)
+                {
+                    this.transform.position += groundPosition - lastGroundPosition;
+                }
+                lastGroundName = groundName;
+                lastGroundPosition = groundPosition;
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        player = this.GetComponent<CharacterController>();
+        Gizmos.DrawWireSphere(transform.position, player.height / 4.2f);
+    }
+}
